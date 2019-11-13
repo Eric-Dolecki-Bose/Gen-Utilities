d42dc5e052d001cab95928f8dfce92de48eeaf74
Eric Dolecki <eric_dolecki@bose.com>
2019-11-13 09:49:38 -0500

    Added console output as comments.

--- Gen-Utilities/ViewController.swift
+++ Gen-Utilities/ViewController.swift
@@ -19,16 +19,16 @@ class ViewController: UIViewController {
         // Some simle examples.
         
         let timeago = 2.days.earlier
-        print("time ago \(timeago.timeAgoSinceNow)")
-        print("time ago \(timeago.shortTimeAgoSinceNow)")
-        print("2 days ago was \(timeago.format(with: .medium))")
+        print("time ago \(timeago.timeAgoSinceNow)")                // time ago 2 days ago
+        print("time ago \(timeago.shortTimeAgoSinceNow)")           // time ago 2d
+        print("2 days ago was \(timeago.format(with: .medium))")    // 2 days ago was Nov 11, 2019
         
         let now = Date()
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy MM dd HH:mm:ss.SSS"
         let birthday = formatter.date(from: "2020 03 28 14:50:12.000")!
-        print(birthday.format(with: .full))
-        let chunk = now.chunkBetween(date: birthday)
+        print(birthday.format(with: .full))                         // Saturday, March 28, 2020
+        let chunk = now.chunkBetween(date: birthday)                // TimeChunk(seconds: 7, minutes: 2, hours: 5, days: 15, weeks: 0, months: 4, years: 0)
         print(chunk)
     }
 }
